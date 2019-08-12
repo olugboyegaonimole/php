34873e8b9917e22b9cc9ad172611954c3519b3de
olugboyegaonimole <39015008+olugboyegaonimole@users.noreply.github.com>
2019-08-12 16:42:58 +0100

    Add files via upload

---
+++ views/home.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <div class="row">
+
+
+
+        <div class="col-3 p-5">
+
+            <img src="/svg/city.jpg" style="max-height: 200px" class="rounded-circle">
+
+        </div>
+
+
+
+        <div class="col-9 p-5">
+
+            <div><h1>testuser1</h1></div>
+
+
+            <div class="d-flex">
+                <div class="pr-5"><strong>56</strong> posts </div>
+                <div class="pr-5"><strong>89k</strong> followers </div>
+                <div><strong>121k</strong> following </div>
+            </div>
+
+
+            <div class="pt-5 font-weight-bold"><strong>onimolefamily.org</strong></div>
+
+
+            <div>This is the freecodegram page for testuser1</div>
+
+
+            <div class="pt-5"><a href="" >www.testuser1.com</a></div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+    <div class="row pt-4">
+
+            <div class="col-4">
+                <img src="/svg/city2.jpg" class="w-100">
+            </div>
+
+            <div class="col-4">
+                <img src="/svg/city2.jpg" class="w-100">
+            </div>
+
+            <div class="col-4">
+                <img src="/svg/city2.jpg" class="w-100">
+            </div>
+
+    </div>
+
+
+
+</div>
+@endsection
